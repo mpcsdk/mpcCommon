b6efc833020a4e6a02cee7e51d3f29d55ac65c5c
frank <no>
2024-04-11 10:59:16 +0800

    tidy

--- mpcdao/risk_ctrl.go
+++ mpcdao/risk_ctrl.go
@@ -28,11 +28,11 @@ func NewRiskCtrlRule(redis *gredis.Redis, dur int) *RiskCtrlRule {
 }
 
 // /
-func (s *RiskCtrlRule) GetContractAbiBriefs(ctx context.Context, ChainId string, kind string) ([]*entity.Contractabi, error) {
+func (s *RiskCtrlRule) GetContractAbiBriefs(ctx context.Context, ChainId int, kind string) ([]*entity.Contractabi, error) {
 	// model := g.Model(dao.Contractabi.Table()).Ctx(ctx).Cache(gdb.CacheOption{
 	model := dao.Contractabi.Ctx(ctx).Cache(gdb.CacheOption{
 		Duration: s.dur,
-		Name:     dao.Contractabi.Table() + ChainId + kind,
+		Name:     dao.Contractabi.Table() + strconv.Itoa(ChainId) + kind,
 		Force:    true,
 	}).Fields(
 		dao.Contractabi.Columns().ChainId,
@@ -40,7 +40,7 @@ func (s *RiskCtrlRule) GetContractAbiBriefs(ctx context.Context, ChainId string,
 		dao.Contractabi.Columns().ContractName,
 		dao.Contractabi.Columns().ContractKind,
 	)
-	if ChainId != "" {
+	if ChainId >= 0 {
 		model = model.Where(dao.Contractabi.Columns().ChainId, ChainId)
 	}
 	if kind != "" {
@@ -58,7 +58,7 @@ func (s *RiskCtrlRule) GetContractAbiBriefs(ctx context.Context, ChainId string,
 
 // /
 
-func (s *RiskCtrlRule) GetContractAbi(ctx context.Context, ChainId string, address string, flush bool) (*entity.Contractabi, error) {
+func (s *RiskCtrlRule) GetContractAbi(ctx context.Context, ChainId int, address string, flush bool) (*entity.Contractabi, error) {
 	rst, err := dao.Contractabi.Ctx(ctx).Cache(gdb.CacheOption{
 		Duration: func() time.Duration {
 			if flush {
@@ -67,7 +67,7 @@ func (s *RiskCtrlRule) GetContractAbi(ctx context.Context, ChainId string, addre
 				return s.dur
 			}
 		}(),
-		Name:  dao.Contractabi.Table() + ChainId + address,
+		Name:  dao.Contractabi.Table() + strconv.Itoa(ChainId) + address,
 		Force: true,
 	}).
 		Where(dao.Contractabi.Columns().ChainId, ChainId).
@@ -84,10 +84,10 @@ func (s *RiskCtrlRule) ClearContractRuleCache(ctx context.Context,
 	ChainId, kind string, address string) {
 	g.DB().GetCore().GetCache().Remove(ctx, "SelectCache:"+dao.Contractrule.Table()+ChainId+kind+address)
 }
-func (s *RiskCtrlRule) GetContractRuleBriefs(ctx context.Context, ChainId string, kind string) ([]*entity.Contractrule, error) {
+func (s *RiskCtrlRule) GetContractRuleBriefs(ctx context.Context, ChainId int, kind string) ([]*entity.Contractrule, error) {
 	model := dao.Contractrule.Ctx(ctx).Cache(gdb.CacheOption{
 		Duration: s.dur,
-		Name:     dao.Contractrule.Table() + ChainId + kind,
+		Name:     dao.Contractrule.Table() + strconv.Itoa(ChainId) + kind,
 		Force:    true,
 	}).Fields(
 		dao.Contractrule.Columns().ChainId,
@@ -95,7 +95,7 @@ func (s *RiskCtrlRule) GetContractRuleBriefs(ctx context.Context, ChainId string
 		dao.Contractrule.Columns().ContractName,
 		dao.Contractrule.Columns().ContractKind,
 	)
-	if ChainId != "" {
+	if ChainId >= 0 {
 		model = model.Where(dao.Contractrule.Columns().ChainId, ChainId)
 	}
 	if kind != "" {
@@ -112,7 +112,7 @@ func (s *RiskCtrlRule) GetContractRuleBriefs(ctx context.Context, ChainId string
 }
 
 // /
-func (s *RiskCtrlRule) GetContractRule(ctx context.Context, ChainId string, address string, flush bool) (*entity.Contractrule, error) {
+func (s *RiskCtrlRule) GetContractRule(ctx context.Context, ChainId int, address string, flush bool) (*entity.Contractrule, error) {
 	rst, err := dao.Contractrule.Ctx(ctx).Cache(gdb.CacheOption{
 		Duration: func() time.Duration {
 			if flush {
@@ -121,7 +121,7 @@ func (s *RiskCtrlRule) GetContractRule(ctx context.Context, ChainId string, addr
 				return s.dur
 			}
 		}(),
-		Name:  dao.Contractrule.Table() + ChainId + address,
+		Name:  dao.Contractrule.Table() + strconv.Itoa(ChainId) + address,
 		Force: true,
 	}).Where(dao.Contractrule.Columns().ChainId, ChainId).
 		Where(dao.Contractrule.Columns().ContractAddress, address).One()

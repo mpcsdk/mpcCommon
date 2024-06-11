63a5db074e028b15f0fd6d10c66695c95b38f382
frank <no>
2024-06-11 11:14:39 +0800

    mpc walletaddr

--- mpcdao/mpc.go
+++ mpcdao/mpc.go
@@ -10,6 +10,8 @@ import (
 	"github.com/gogf/gf/v2/os/gcache"
 	"github.com/mpcsdk/mpcCommon/mpccode"
 	"github.com/mpcsdk/mpcCommon/mpcdao/dao"
+	"github.com/mpcsdk/mpcCommon/mpcdao/model/do"
+	"github.com/mpcsdk/mpcCommon/mpcdao/model/entity"
 )
 
 type MpcContext struct {
@@ -17,14 +19,88 @@ type MpcContext struct {
 	dur   time.Duration
 }
 
-func (s *MpcContext) ExistsMpcAddr(ctx context.Context, addr string) (bool, error) {
-	cnt, err := dao.MpcContext.DB().Model(dao.MpcContext.Table()).Ctx(ctx).Cache(gdb.CacheOption{
+// ///context
+func (s *MpcContext) UpdateContext(ctx context.Context, userId string, data *do.MpcContext) error {
+	_, err := dao.MpcContext.Ctx(ctx).Cache(gdb.CacheOption{
+		Duration: -1,
+		Name:     dao.MpcContext.Table() + userId,
+		Force:    false,
+	}).Data(data).Where(do.MpcContext{
+		UserId: userId,
+	}).Update()
+	return err
+}
+
+func (s *MpcContext) InertContext(ctx context.Context, userId string, data *do.MpcContext) error {
+	cnt, err := dao.MpcContext.Ctx(ctx).Where(do.MpcContext{
+		UserId: userId,
+	}).CountColumn(dao.MpcContext.Columns().UserId)
+	if err != nil {
+		return err
+	}
+	if cnt != 0 {
+		return nil
+	}
+
+	_, err = dao.MpcContext.Ctx(ctx).Cache(gdb.CacheOption{
+		Duration: -1,
+		Name:     dao.MpcContext.Table() + userId,
+		Force:    false,
+	}).Data(data).
+		Insert()
+
+	return err
+}
+func (s *MpcContext) FetchContext(ctx context.Context, userId string) (*entity.MpcContext, error) {
+	var data *entity.MpcContext
+	if userId == "" {
+		return nil, nil
+	}
+	rst, err := dao.MpcContext.Ctx(ctx).Cache(gdb.CacheOption{
+		Duration: time.Hour,
+		Name:     dao.MpcContext.Table() + userId,
+		Force:    false,
+		// }).Where("user_id", 1).One()
+	}).Where(do.MpcContext{
+		UserId: userId,
+	}).One()
+	if err != nil {
+		return nil, mpccode.CodeInternalError()
+	}
+
+	err = rst.Struct(&data)
+	g.Log().Debug(ctx, "FetchContext data:", data)
+
+	return data, err
+}
+
+/////
+
+// ///walletaddr
+func (s *MpcContext) InsertWalletAddr(ctx context.Context, userId string, addr string, chainId int64) error {
+	_, err := dao.WalletAddr.DB().Model(dao.WalletAddr.Table()).Ctx(ctx).Cache(gdb.CacheOption{
 		Duration: s.dur,
-		Name:     dao.MpcContext.Table() + addr,
+		Name:     dao.WalletAddr.Table() + userId + addr,
 		Force:    true,
-	}).Where(dao.MpcContext.Columns().PubKey, addr).Count()
+	}).Data(&entity.WalletAddr{
+		UserId:     userId,
+		ChainId:    chainId,
+		WalletAddr: addr,
+	}).Insert()
 	if err != nil {
 		g.Log().Error(ctx, "ExistsMpcAddr:", "addr", addr, "err", err)
+		return mpccode.CodeInternalError()
+	}
+	return nil
+}
+func (s *MpcContext) ExistsWalletAddr(ctx context.Context, addr string) (bool, error) {
+	cnt, err := dao.WalletAddr.DB().Model(dao.WalletAddr.Table()).Ctx(ctx).Cache(gdb.CacheOption{
+		Duration: s.dur,
+		Name:     dao.WalletAddr.Table() + addr,
+		Force:    true,
+	}).Where(dao.WalletAddr.Columns().WalletAddr, addr).Count()
+	if err != nil {
+		g.Log().Error(ctx, "ExistsWalletAddr:", "addr", addr, "err", err)
 		return false, mpccode.CodeInternalError()
 	}
 
@@ -36,8 +112,10 @@ func (s *MpcContext) ExistsMpcAddr(ctx context.Context, addr string) (bool, erro
 
 func NewMcpContet(redis *gredis.Redis, dur int) *MpcContext {
 	// dao.MpcContext.DB().GetCache().SetAdapter(gcache.NewAdapterRedis(redis))
-
-	g.DB(dao.MpcContext.Group()).GetCache().SetAdapter(gcache.NewAdapterRedis(redis))
+	if redis != nil {
+		g.DB(dao.MpcContext.Group()).GetCache().SetAdapter(gcache.NewAdapterRedis(redis))
+		g.DB(dao.WalletAddr.Group()).GetCache().SetAdapter(gcache.NewAdapterRedis(redis))
+	}
 	return &MpcContext{
 		redis: redis,
 		dur:   time.Duration(dur) * time.Second,
